d6f587a66141d4020b5cc854fc7e57049526ce80
e-freak <maxjoehnk@gmail.com>
2013-05-03 22:04:46 +0200

    Switched to synchronous PreLoginEvent

--- NewAgeAuth/src/main/java/org/hivebuild/newageauth/LoginHandler.java
+++ NewAgeAuth/src/main/java/org/hivebuild/newageauth/LoginHandler.java
@@ -6,9 +6,9 @@ import java.util.HashMap;
 
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
-import org.bukkit.event.player.AsyncPlayerPreLoginEvent.Result;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerPreLoginEvent;
+import org.bukkit.event.player.PlayerPreLoginEvent.Result;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 public class LoginHandler implements Listener {
@@ -26,14 +26,13 @@ public class LoginHandler implements Listener {
 	}
 	
 	@EventHandler
-	public void onPlayerPreLogin(AsyncPlayerPreLoginEvent event) {
+	public void onPlayerPreLogin(PlayerPreLoginEvent event) {
 		System.out.println("onPlayerPreLogin-1");
 		if (players.containsKey(event.getName())) {
-			event.setLoginResult(Result.KICK_OTHER);
-			event.setKickMessage(io.translate("Kick.AlreadyOnline"));
+			event.disallow(Result.KICK_OTHER, io.translate("Kick.AlreadyOnline"));
 			return;
 		}
-		/*try {
+		try {
 			ResultSet rs = db.query("SELECT * FROM `player` WHERE `ip`='" + event.getAddress() + "';");
 			if (rs != null) {
 				int i = 0;
@@ -46,25 +45,16 @@ public class LoginHandler implements Listener {
 			}
 		}catch(SQLException e) {
 			if (config.getDebug()) e.printStackTrace();
-		}*/
+		}
 		System.out.println("onPlayerPreLogin-2");
 		System.out.println(event.getName());
 		AuthPlayer player = new AuthPlayer(event.getName(), event.getAddress().getHostAddress());
 		players.put(player.getName(), player);
 		if (player.isPremium()) {
-			if (player.isCracked()) {
-				event.setLoginResult(Result.KICK_OTHER);
-				event.setKickMessage(io.translate("Kick.CrackedPremiumUser"));
-			}else event.setLoginResult(Result.ALLOWED);
+			if (player.isCracked()) event.disallow(Result.KICK_OTHER, io.translate("Kick.CrackedPremiumUser"));
 		}else {
-			if (!player.isIPValid() && !config.getBoolean("Key.Enabled")) {
-				event.setLoginResult(Result.KICK_OTHER);
-				event.setKickMessage(io.translate("Kick.CrackedMultiIP"));
-			}
-			if (!player.isUsernameValid() && !config.getBoolean("Key.Enabled")) {				
-				event.setLoginResult(Result.KICK_OTHER);
-				event.setKickMessage(io.translate("Kick.CrackedMultiUsers"));
-			}
+			if (!player.isIPValid() && !config.getBoolean("Key.Enabled")) event.disallow(Result.KICK_OTHER, io.translate("Kick.CrackedMultiIP"));
+			if (!player.isUsernameValid() && !config.getBoolean("Key.Enabled")) event.disallow(Result.KICK_OTHER, io.translate("Kick.CrackedMultiUsers"));				
 		}
 	}
 
